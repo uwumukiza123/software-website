f5ccb63211fe3a39c600fff153e8a9dfb00a82bd
uwumukiza123 <uwumukiza123>
2026-02-27 17:17:57 +0200

    feat: add hamburger menu

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,15 +2,25 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between">
-      <Image
-        src={"../../images/logo 1.svg"}
-        alt={"logo"}
-        width={197}
-        height={83}
-      />
+    <div className="md:flex items-center justify-between">
+      <div className="">
+        <Image
+          src={"../../images/logo 1.svg"}
+          alt={"logo"}
+          width={197}
+          height={83}
+        />
+        <Image
+          src={"../../icons/LucideAlignJustify.svg"}
+          alt={"hamburger menu icon"}
+          width={128}
+          height={128}
+          className="block md:hidden"
+        />
+      </div>
+
       <div>
-        <ul className="flex gap-10 text-black">
+        <ul className="md:flex gap-10 text-black">
           <li>How to start</li>
           <li>Services</li>
           <li>Technologies</li>
